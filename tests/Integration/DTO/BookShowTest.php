1188f63af4363400ffbc636273fc57f63a9d49f2
5ylv41n77 <5ylv41n77@gmail.com>
2020-03-08 17:51:30 +0100

    Tests - add user fixtures

--- tests/Integration/DTO/BookShowTest.php
+++ tests/Integration/DTO/BookShowTest.php
@@ -4,21 +4,27 @@
 namespace App\Tests\Integration\DTO;
 
 use App\DataFixtures\BookFixtures;
+use App\DataFixtures\UserFixtures;
 use App\DTO\BookDTO;
 use App\DTO\BookShow;
 use App\Entity\Book;
 use App\Repository\BookRepository;
 use App\Tests\Integration\FixtureAwareTestCase;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class BookShowTest extends FixtureAwareTestCase
 {
     private const BOOK_UUID_RANDOM = 'ISBN-13:978-013708107';
     private EntityManagerInterface $entityManager;
+    private UserPasswordEncoderInterface $userPasswordEncoder;
 
     public function setUp()
     {
         parent::setUp();
+        self::bootKernel();
+        $this->userPasswordEncoder = self::$container->get(UserPasswordEncoderInterface::class);
+        $this->addFixture(new UserFixtures($this->userPasswordEncoder));
         $this->addFixture(new BookFixtures());
         $this->executeFixtures();
         $this->entityManager = self::$kernel->getContainer()->get('doctrine')->getManager();
