899489e9c8d2bdee5a11f7bfe57dbc9ed89c1781
5ylv41n77 <5ylv41n77@gmail.com>
2020-03-08 12:12:31 +0100

    Migration - add deletedAt property

--- src/Migrations/Version20200124165707.php
+++ src/Migrations/Version20200124165707.php
@@ -22,7 +22,7 @@ final class Version20200124165707 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE book ADD created_by VARCHAR(255) DEFAULT NULL, ADD updated_by VARCHAR(255) DEFAULT NULL, ADD deleted_by VARCHAR(255) DEFAULT NULL, ADD deleted_at DATETIME DEFAULT NULL, ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE book ADD deleted_at DATETIME DEFAULT NULL, ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20200124165707 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE book DROP created_by, DROP updated_by, DROP deleted_by, DROP deleted_at, DROP created_at, DROP updated_at');
+        $this->addSql('ALTER TABLE book DROP deleted_at, DROP created_at, DROP updated_at');
     }
 }
